a5ab62b0c0c274ce86c19ea9d989e3eebc170e9b
juber <juberk1010@gmail.com>
2023-11-02 13:08:50 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  // Other Vite configuration options...
+  build: {
+    rollupOptions: {
+      input: '/src/main.jsx',
+    },
+    // Explicitly include dependencies that should be pre-bundled
+    optimizeDeps: {
+      include: [
+        'react',
+        'react-dom',
+        // Other dependencies...
+      ],
+    },
+  },
+};
