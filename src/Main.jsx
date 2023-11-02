23ccb1d80012230fe06d67a6328de57ef7ab44f4
juber <juberk1010@gmail.com>
2023-11-02 23:46:27 +0530

    fix

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <>
-//     <App />
-//   </>
-// )
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-    <div>
-    <App/>
-    </div>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 )
+
--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  // Other Vite configuration options...
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx',
-    },
-    // Explicitly include dependencies that should be pre-bundled
-    optimizeDeps: {
-      include: [
-        'react',
-        'react-dom',
-        // Other dependencies...
-      ],
-    },
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+})
